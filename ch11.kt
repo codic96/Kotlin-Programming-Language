90cf9486b29350b98c2d8f96d078e5ece1298e14
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:03:53 +0530

    Add files via upload

---
+++ ch11.kt
@@ -0,0 +1,42 @@
+/** Super Keyword  **/
+
+/*open class Programmer1{
+    // Properties
+    open  var s1:String = "Java Programming " //Overriding Properties
+    var s2:String = "Python Programming"
+
+    //Member function
+
+    fun disp()
+    {
+        println("Server Script: $s1")
+        println("Web Development: $s2")
+    }
+
+}
+class Hacker1:Programmer1()
+{
+    //Properties
+    var coder:String = "HTML"
+    override  var s1:String = "Network Programming" //Overriding properties and functions
+    var fcar:String = super.s1
+
+
+    //Member function
+    fun show()
+    {
+        println("The Code Name are: $coder")
+        println("The :$s1")
+        println("The name are : $s2")
+        println("The fcar : $fcar")
+    }
+}
+fun main()
+{
+    val s3 = Programmer1()
+    val s4 = Hacker1()
+    s4.show()
+    s3.disp()
+}
+
+ */
