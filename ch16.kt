682f97251604b60019e69e68ab4ee3ef5506252d
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:05:20 +0530

    Add files via upload

---
+++ ch16.kt
@@ -0,0 +1,14 @@
+/** Data Class **/
+//where your need to create a class solely to hold data
+
+/*data class Employee(val name:String, val age:Int)
+
+fun main()
+{
+    val emp = Employee("Sonam",13)
+    println(emp.name)
+    println(emp.age)
+}
+
+ */
+
