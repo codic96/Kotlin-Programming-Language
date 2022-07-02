45987d80eca47cef631f86fbd1b7a82ae19ffdab
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:03:24 +0530

    Add files via upload

---
+++ ch9.kt
@@ -0,0 +1,45 @@
+/** Inheritance with Primary  & Secondary Constructor **/
+
+/*open class Father3(_car:String,_money:Int){
+    //Properties
+    var car:String=_car
+    var money:Int=_money
+
+
+    //Member function
+    fun disp()
+    {
+        println("Father car : $car")
+        println("Father Money : $money")
+
+    }
+}
+
+class Son1:Father3{
+    //Properties
+    var bike:String
+    // secondary constructor
+
+    constructor(car:String,money:Int,bike:String):super(car,money)
+    {
+        this.bike=bike
+
+    }
+    //Member function
+    fun show()
+    {
+        println("Son's Bike : $bike")
+        println("Father car: $car")
+        println("Father Money: $money")
+
+    }
+}
+
+fun main()
+{
+    val s1 = Son1("Alt 100",1000,"k10")
+    s1.show()
+}
+
+ */
+
