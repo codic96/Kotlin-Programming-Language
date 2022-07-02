bda199912045035ff81199b892ae9cfe880f8a3f
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:04:40 +0530

    Add files via upload

---
+++ ch14.kt
@@ -0,0 +1,49 @@
+/** Interface **/
+
+/*interface Father8
+{
+    //Properties
+    var car:String  //Abstract Property
+
+    //Member function
+    fun disp()
+    {
+        println("Car are: $car")
+    }
+
+    fun hello()  //Abstract Method
+
+
+}
+class Son8:Father8{
+    //Properties
+    var bike:String = "k10"
+
+    //Implementing Father's Abstract Property
+    override var car:String = "Alt 100"
+
+    //Member function
+
+    fun show()
+    {
+        println("Father's car: $car")
+    }
+
+    //Implementing Hello Abstract Method
+    override fun hello()
+    {
+        println("Father's Abstract Method Hello")
+
+    }
+
+}
+fun main()
+{
+    val s1= Son8()
+    s1.show()
+    s1.disp()
+    s1.hello()
+
+}
+
+ */
