6e38ad2dfd1b4d0ec952e5c1a420aa45a43b8ba0
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:02:40 +0530

    Add files via upload

---
+++ ch6.kt
@@ -0,0 +1,28 @@
+/*** Getter and setter ***/
+
+/*class User(_id:Int,_name:String,_age:Int)
+{
+    val id:Int = _id
+    get() = field
+
+    var name:String = _name
+    get() = field
+    set(value)
+    {
+        field = value
+    }
+
+    var age:Int = _age
+    get() = field
+    set(value)
+    {
+        field = value
+
+    }
+}
+fun main()
+{
+    val u1 = User(_id:1,_name:"Sonam",_age:27)
+}
+
+ */
