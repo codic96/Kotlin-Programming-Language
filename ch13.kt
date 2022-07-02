6e59446735e1636e26c91bb77224eb580545b99f
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:04:24 +0530

    Add files via upload

---
+++ ch13.kt
@@ -0,0 +1,43 @@
+/** Abstract class & Method **/
+
+ /*open abstract  class Father7  //create class abstract
+{
+    //Properties
+    var car:String ="Alto 100"
+
+    //Member function
+    fun disp(){
+        println("Father car's : $car")
+
+    }
+    //Abstract method
+
+   abstract fun hello() //create a abstract function
+}
+ class Son7:Father7(){
+    //Member function
+
+    fun show(){
+        println("The car display : $car")
+    }
+     //Implementation of Father's Abstract Method
+     override fun hello()
+     {
+         println("Father abstract method")
+     }
+
+}
+
+fun main()
+{
+    val f1 = Son7()
+    f1.show()
+    f1.hello()
+    /*val f2 = Father7() //can't create object of abstract class
+    f2.disp()*/
+
+
+}
+
+  */
+
