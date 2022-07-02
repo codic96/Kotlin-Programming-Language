a730e1b28919a6873204b86faabee763ce6a8cb8
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:03:38 +0530

    Add files via upload

---
+++ ch10.kt
@@ -0,0 +1,41 @@
+/** Overriding Properties and Functions **/
+
+/*open class Programmer{
+    // Properties
+   open  var s1:String = "Java Programming " //Overriding Properties
+    var s2:String = "Python Programming"
+
+    //Member function
+
+    fun disp()
+    {
+        println("Server Script: $s1")
+        println("Web Development: $s2")
+    }
+
+}
+class Hacker:Programmer()
+{
+    //Properties
+    var coder:String = "HTML"
+    override  var s1:String = "Network Programming" //Overriding properties and functions
+
+
+    //Member function
+    fun show()
+    {
+        println("The Code Name are: $coder")
+        println("The :$s1")
+        println("The name are : $s2")
+    }
+}
+fun main()
+{
+    val s3 = Programmer()
+    val s4 = Hacker()
+    s4.show()
+    s3.disp()
+}
+
+
+ */
