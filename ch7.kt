92f960f23f5fe6c3c51264badc4cc42bd1adedca
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-07-02 09:02:55 +0530

    Add files via upload

---
+++ ch7.kt
@@ -0,0 +1,56 @@
+/** Inheritance **/
+
+/*open class Inter{ //Open inheritance
+    // Properties
+    var car:String = "Alt 100"
+    var money:Int = 1000
+
+    //Member function
+
+    fun disp()
+    {
+        println("The car name are: $car")
+        println("The Father name are: $money")
+    }
+}
+
+
+class Son:Inter() //create inheritance
+{
+    //Properties
+    var bike:String = "K 10"
+
+    //Member function
+
+    fun show()
+    {
+        println("The Son's bike : $bike")
+
+    }
+}
+class Daughter:Inter()
+{
+    //Properties
+    var name:String = "Priya"
+
+    //Member function
+    fun name()
+    {
+        println("The name are : $name")
+    }
+}
+fun main()
+{
+    val f1 = Inter()
+    f1.disp()
+    val f2 = Son()
+    f2.show()
+    f2.disp()
+    val f3 = Daughter()
+    f3.name()
+    f3.disp()
+}
+
+ */
+
+
